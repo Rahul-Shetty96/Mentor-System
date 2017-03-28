17d9b84edf7107ea523dc1d79ff182198161dd55
rajramch <r.ramchandani.rj@gmail.com>
2017-03-28 19:21:25 +0530

    made basic structure for mentor page

---
+++ html/mentorpage.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="utf-8" http-equiv="encoding">
+	<title>Welcome</title>
+	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+	<script src="../js/jquery.js" type="text/javascript"></script>
+	<script src="../bootstrap/js/bootstrap.min.js"></script>
+	<script src="../js/admin1.js" type="text/javascript"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-filestyle/1.2.1/bootstrap-filestyle.min.js"></script>
+	<link href="../css/header.css" rel="stylesheet">
+	<link href="../css/admin1.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css" />
+</head>
+<body>
+
+<?php require 'header.php'; 
+	  require '../php/dp.php';
+?>
+
+<div class="container">
+  <h2>Hello, (Teacher Name)</h2>
+  <h3>Your Mentees<h3>
+<h4>D10<h4> 
+  <table class="table table-condensed">
+    <thead>
+      <tr>
+        <th>Student Name</th>
+        <th>Last Meet</th>
+        <th>Issue Discussed</th>
+		<th>Next Meet<th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+	  <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+    </tbody>
+  </table>
+  <br>
+  <h4>D15<h4>
+  <table class="table table-condensed">
+    <thead>
+      <tr>
+        <th>Student Name</th>
+        <th>Last Meet</th>
+        <th>Issue Discussed</th>
+		<th>Next Meet<th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+	  <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+    </tbody>
+  </table>
+  <br>
+  <h4>D20<h4>
+  <table class="table table-condensed">
+    <thead>
+      <tr>
+        <th>Student Name</th>
+        <th>Last Meet</th>
+        <th>Issue Discussed</th>
+		<th>Next Meet<th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+	  <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
