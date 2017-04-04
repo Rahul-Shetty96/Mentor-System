d3fe75e8783b0917d98bdbd6f50c898662dbdd8c
PawanTilwani <pawan.tilwani@ves.ac.in>
2017-04-04 17:57:32 +0530

    footer added

---
+++ html/footer.php
@@ -0,0 +1,14 @@
+<div class="push"></div>
+	
+</div>
+
+
+
+<div class="footer">
+	<span class="copyright">
+	 Copyright&copy 2017
+	</span>
+	<span class="developed">
+		Developed by Group 9
+	</span>
+ </div>
